da42d918e1a2e047bd8d0c448e3ef9bd773f5a85
Jayden Everett <everjr1@student.op.ac.nz>
2024-09-27 14:47:56 +1200

    End of tearm friday

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let year = 2024;
+
+    let currentTime = new DataTransfer();
+</script>
+
+<div>
+    <!--<img src={pic} alt={picname}-->
+    <p>&copy; Web 1 {year}: the time is {currentTime}</p>
+</div>
+
+
+
+<style>
+    p{
+        background-color: aqua;
+        padding: 2em;
+        font-weight: bold;
+
+        
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Footer from '$lib/Footer.svelte';
+    import dog1 from '$lib/pics/dog1.png';
+</script>
+
+<h1>Welcome to your library project...</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Footer year={2023} pic={dog1} picname={"dog"}/>
+<Footer year={1999}/>
+<Footer />
+<ooter />
+
+<style>
+    :global(*) {
+        font-family: sans-serif;
+    }
+</style>
